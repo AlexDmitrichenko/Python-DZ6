2c400796c10c7c86069626b69c74b9be42a763ce
Alex <best1972@list.ru>
2023-03-09 16:32:58 +0300

    made tasks 32

--- Tasks.py
+++ Tasks.py
@@ -18,4 +18,19 @@
 
 # print(newArray)
 
+#**************************************************************************************
+# Задача 32: Определить индексы элементов массива (списка), значения которых принадлежат 
+# заданному диапазону (т.е. не меньше заданного минимума и не больше заданного максимума)
+# Пример Ввод: [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+# Вывод: [1, 9, 13, 14, 19]
 
+# from random import randint
+
+# array = [randint(-10,10) for i in range(int(input("Pls enter the length of the array: ")))]
+
+# print(array)
+
+# minRange = int(input("Pls enter the minimum of the range: "))
+# maxRange = int(input("Pls enter the maximum of the range: "))
+
+# print([i for i in range(len(array)) if array[i]>=minRange and array[i]<=maxRange])
